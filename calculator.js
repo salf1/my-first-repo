a8142624be5d24bb34eb6b2c9354fb9b35292e6d
salf1 <164863417+salf1@users.noreply.github.com>
2026-09-10 21:25:53 +0300

    Add readline interface for input
    Added readline interface for user input.

--- calculator.js
+++ calculator.js
@@ -1,1 +1,10 @@
 console.log("Добро пожаловать в калькулятор!");
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Добро пожаловать в калькулятор!");
