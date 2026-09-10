79cb9f9cc8dd164e5027d06a6cc139ae380b60a7
salf1 <164863417+salf1@users.noreply.github.com>
2026-09-10 21:26:06 +0300

    Add first number input

--- calculator.js
+++ calculator.js
@@ -8,3 +8,17 @@ const rl = readline.createInterface({
 });
 
 console.log("Добро пожаловать в калькулятор!");
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Добро пожаловать в калькулятор!");
+
+rl.question("Введите первое число: ", (a) => {
+  console.log(`Вы ввели: ${a}`);
+  rl.close();
+});
