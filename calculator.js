a0c4b051caeb6a5a54a9db734a048254f70ee5c4
salf1 <164863417+salf1@users.noreply.github.com>
2026-09-10 21:26:22 +0300

    Add operator input

--- calculator.js
+++ calculator.js
@@ -22,3 +22,18 @@ rl.question("Введите первое число: ", (a) => {
   console.log(`Вы ввели: ${a}`);
   rl.close();
 });
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Добро пожаловать в калькулятор!");
+
+rl.question("Введите первое число: ", (a) => {
+  rl.question("Введите оператор (+, -, *, /): ", (op) => {
+    console.log(`Число: ${a}, оператор: ${op}`);
+    rl.close();
+  });
+});
