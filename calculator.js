6f61a7f114bac22b9589316e39ac7232ffd522d8
salf1 <164863417+salf1@users.noreply.github.com>
2026-09-12 19:49:07 +0300

    Fix indentation

--- calculator.js
+++ calculator.js
@@ -16,7 +16,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-console.log("Добро пожаловать в калькулятор!");
+console.log("Добро пожаловать в калькулятор");
 
 rl.question("Введите первое число: ", (a) => {
   console.log(`Вы ввели: ${a}`);
